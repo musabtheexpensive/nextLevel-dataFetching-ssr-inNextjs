2ebd9518092c856b1325372418bffe730b909fca
musabtheexpensive <musabalmahi53@gmail.com>
2024-01-20 10:15:28 +0600

    very easily created dynamic card when clicking the see more button

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,10 +1,29 @@
+import Link from "next/link";
 
-const DetailPage = () => {
-    return (
-        <div>
-            <h1>Post Detail Page</h1>
+const DetailPage = async ({ params }) => {
+  console.log(params.id);
+  const res = await fetch(`http://localhost:5001/posts/${params.id}`);
+  const post = await res.json();
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Post Detail Page</h1>
+      <div
+        key={post.id}
+        className="card  bg-gray-200 shadow-xl w-[70%] my-5 mx-auto"
+      >
+        <div className="card-body">
+          <h2 className="card-title">{post.title}</h2>
+          <p>{post.description}</p>
+          <p>Likes : {post.likesCount}</p>
         </div>
-    );
+        <div className="card-actions justify-end m-6">
+          <Link href="/posts">
+            <button className="btn btn-accent">Back</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailPage;
