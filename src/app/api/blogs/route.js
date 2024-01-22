463ee44382a51efc2c7a7c6eeeefe1901ea97238
Musab Al Mahi <musabalmahi53@gmail.com>
2024-01-22 16:42:17 +0600

    All things learned so far are re-explored

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -3,3 +3,15 @@ import { NextResponse } from "next/server";
 export const GET = () => {
   return NextResponse.json({ message: "Hello, World" });
 };
+
+// post api
+export const POST = () => {};
+
+// PATCH api
+export const PATCH = () => {};
+
+// DELETE api
+export const DELETE = () => {};
+
+// PUT API
+export const PUT = () => {};
